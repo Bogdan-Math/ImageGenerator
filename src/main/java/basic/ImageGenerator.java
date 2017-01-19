21c59d820e1a012f475dc0719e14a0f142aac140
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2017-01-19 17:59:15 +0200

    add try/catch with throwing RuntimeException instead of checked exception

--- src/main/java/basic/ImageGenerator.java
+++ src/main/java/basic/ImageGenerator.java
@@ -6,25 +6,38 @@ import java.io.*;
 
 public class ImageGenerator {
 
-    public void copyImage(File fromFile, File toFile, String outputFormat) throws IOException {
+    public void copyImage(File fromFile, File toFile, String outputFormat) {
+        try {
             ImageIO.write(fileToBufferedImage(fromFile), outputFormat, toFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public BufferedImage fileToBufferedImage(File fromFile) throws IOException {
         return ImageIO.read(fromFile);
     }
 
-    public byte[] bufferedImageToByteArray(BufferedImage bufferedImage, String format) throws IOException {
+    public byte[] bufferedImageToByteArray(BufferedImage bufferedImage, String format) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(bufferedImage, format, baos);
-        baos.flush();
-        byte[] imageInBytes = baos.toByteArray();
-        baos.close();
-        return imageInBytes;
+        try {
+            ImageIO.write(bufferedImage, format, baos);
+            baos.flush();
+            byte[] imageInBytes = baos.toByteArray();
+            baos.close();
+            return imageInBytes;
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public BufferedImage byteArrayToBufferedImage(byte[] imageInBytes) throws IOException {
-        return ImageIO.read(new ByteArrayInputStream(imageInBytes));
+    public BufferedImage byteArrayToBufferedImage(byte[] imageInBytes) {
+        try {
+            return ImageIO.read(new ByteArrayInputStream(imageInBytes));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
