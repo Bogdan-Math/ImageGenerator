4fac0828970ba2401fed56fe3990f26cfc74a26d
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2017-05-24 10:53:02 +0300

    add upload and show image

--- src/main/java/web/vaadin/MyUI.java
+++ src/main/java/web/vaadin/MyUI.java
@@ -1,40 +1,71 @@
 package web.vaadin;
 
-import basic.ObjectTypeConverter;
 import com.vaadin.annotations.Theme;
-import com.vaadin.server.StreamResource;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.FileResource;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
-import com.vaadin.ui.Label;
-import utility.FileReader;
+import com.vaadin.ui.Upload.SucceededEvent;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 @Theme("mytheme")
+@Title("Image Generator")
 public class MyUI extends UI {
 
-    private ObjectTypeConverter typeConverter = new ObjectTypeConverter();
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        VerticalLayout layout = new VerticalLayout(new Label("VAADIN"));
+        final Embedded image = new Embedded("Uploaded Image");
+        image.setVisible(false);
 
+        class ImageUploader implements Upload.Receiver, Upload.SucceededListener {
+            public File file;
 
-        Image image = new Image("!!!", new StreamResource(createStreamResource(), "images/canonical.jpg"));
-        layout.addComponent(image);
-        setContent(layout);
-    }
+            public OutputStream receiveUpload(String filename,
+                                              String mimeType) {
+                // Create upload stream
+                FileOutputStream fos = null; // Stream to write to
+                try {
+                    // Open the file for writing.
+                    file = new File("/tmp/uploads/" + filename);
+                    fos = new FileOutputStream(file);
+                } catch (final java.io.FileNotFoundException e) {
+                    new Notification("Could not open file<br/>",
+                            e.getMessage(),
+                            Notification.Type.ERROR_MESSAGE)
+                            .show(Page.getCurrent());
+                    return null;
+                }
+                return fos; // Return the output stream to write to
+            }
 
-    private StreamResource.StreamSource createStreamResource() {
-        return new StreamResource.StreamSource() {
-            @Override
-            public InputStream getStream() {
-                FileReader fileReader = new FileReader();
-                return typeConverter
-                        .inputStreamFromBufferedImage(typeConverter
-                                .bufferedImageFromFile(fileReader
-                                        .getFileObject("images/canonical.jpg")), "jpg");
+            public void uploadSucceeded(SucceededEvent event) {
+                // Show the uploaded file in the image viewer
+                image.setVisible(true);
+                image.setSource(new FileResource(file));
             }
         };
+
+        ImageUploader receiver = new ImageUploader();
+
+        // Create the upload with a caption and set receiver later
+        Upload upload = new Upload("Upload Image Here", receiver);
+        upload.setButtonCaption("Start Upload");
+        upload.addSucceededListener(receiver);
+
+        // Put the components in a panel
+        Panel panel = new Panel("Cool Image Storage");
+        Layout panelContent = new VerticalLayout();
+        panelContent.addComponents(upload, image);
+        image.setWidth("100%");
+//        image.setHeight("25%");
+        panel.setContent(panelContent);
+
+        setContent(panelContent);
+
     }
 
 }
