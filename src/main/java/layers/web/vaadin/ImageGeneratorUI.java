e1b6d5d407e491b2b5e2375031f8fe6c2913ec01
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2017-06-28 12:59:18 +0300

    add style to codacy and github links

--- src/main/java/layers/web/vaadin/ImageGeneratorUI.java
+++ src/main/java/layers/web/vaadin/ImageGeneratorUI.java
@@ -5,7 +5,10 @@ import com.vaadin.annotations.Title;
 import com.vaadin.server.ExternalResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Link;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
@@ -38,19 +41,24 @@ public class ImageGeneratorUI extends UI {
         verticalLayout.addComponents(githubLink, codacyLink, uploadComponent, patternsGroup, slider, images);
 
         verticalLayout.setComponentAlignment(githubLink, Alignment.TOP_RIGHT);
-        verticalLayout.setComponentAlignment(codacyLink, Alignment.TOP_RIGHT);
-        verticalLayout.setComponentAlignment(patternsGroup, Alignment.TOP_CENTER);
+        verticalLayout.setComponentAlignment(codacyLink, Alignment.TOP_LEFT);
         verticalLayout.setComponentAlignment(uploadComponent, Alignment.TOP_CENTER);
+        verticalLayout.setComponentAlignment(patternsGroup, Alignment.TOP_CENTER);
 
         setContent(verticalLayout);
     }
 
     private Link githubLink() {
-        return link("GitHub", "https://github.com/Bogdan-Math/ImageGenerator");
+        Link githubLink = link("", "https://github.com/Bogdan-Math/ImageGenerator");
+        githubLink.setIcon(new ExternalResource("https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67"));
+        githubLink.setStyleName("github-link");
+        return githubLink;
     }
 
     private Link codacyLink() {
-        return link("Codacy", "https://www.codacy.com/app/bogdan-math-stepanov/ImageGenerator/dashboard");
+        Link codacyLink = link("", "https://www.codacy.com/app/bogdan-math-stepanov/ImageGenerator/dashboard");
+        codacyLink.setIcon(new ExternalResource("https://api.codacy.com/project/badge/Grade/433e9ca7250d46f2bbd6280b7db5768c"));
+        return codacyLink;
     }
 
     private Link link(String caption, String sourceURL) {
