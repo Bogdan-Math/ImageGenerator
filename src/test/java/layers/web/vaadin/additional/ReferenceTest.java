68b163473df9ed465865b810c62f7621913c7fa8
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2017-10-16 23:04:27 +0300

    add inner tests to references

---
+++ src/test/java/layers/web/vaadin/additional/ReferenceTest.java
@@ -0,0 +1,40 @@
+package layers.web.vaadin.additional;
+
+import com.vaadin.ui.Link;
+import org.junit.Test;
+
+import static layers.web.vaadin.additional.Reference.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class ReferenceTest {
+
+    @Test
+    public void codacy() throws Exception {
+        Link codacyLink = CODACY.link();
+
+        assertThat(codacyLink.getCaption(), is((String) null));
+    }
+
+    @Test
+    public void github() throws Exception {
+        Link githubLink = GITHUB.link();
+
+        assertThat(githubLink.getCaption(), is((String) null));
+    }
+
+    @Test
+    public void flags() throws Exception {
+        Link flagsLink = FLAGS.link();
+
+        assertThat(flagsLink.getCaption(), is("Thanks for FLAGS!"));
+    }
+
+    @Test
+    public void javarush() throws Exception {
+        Link javarushLink = JAVARUSH.link();
+
+        assertThat(javarushLink.getCaption(), is("Thanks for SKILLS!"));
+    }
+
+}
