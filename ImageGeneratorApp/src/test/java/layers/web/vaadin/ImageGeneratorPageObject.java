b75471963c410313205bc91fdc87f502ea891856
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2017-11-21 17:31:41 +0200

    add single initialization place for settings for integration tests (UI)

--- ImageGeneratorApp/src/test/java/layers/web/vaadin/ImageGeneratorPageObject.java
+++ ImageGeneratorApp/src/test/java/layers/web/vaadin/ImageGeneratorPageObject.java
@@ -3,15 +3,28 @@ package layers.web.vaadin;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 
 import static com.codeborne.selenide.Selenide.$;
+import static java.lang.Integer.valueOf;
 
 public class ImageGeneratorPageObject {
 
-    public URL url() throws MalformedURLException {
-        return new URL("http", "localhost", 9090,"/image-generator-app");
+    public URL url() throws Exception {
+        Properties props = new Properties();
+        URL url          = getClass().getClassLoader()
+                                     .getResource("webapp-integration-tests.properties");
+
+        assert url != null;
+        props.load(url.openStream());
+
+        String protocol = props.getProperty("webapp.protocol");
+        String host     = props.getProperty("webapp.host");
+        Integer port    = valueOf(props.getProperty("webapp.port"));
+        String route    = "/" + props.getProperty("webapp.route");
+
+        return new URL(protocol, host, port, route);
     }
 
     public SelenideElement codacyLink() {
