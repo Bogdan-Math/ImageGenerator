16bc25e3adac8cadf9ea73e06dc51832338deae5
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2017-11-23 12:43:16 +0200

    move properties initialization to spring context

--- ImageGeneratorApp/src/test/java/layers/web/vaadin/ImageGeneratorPageObject.java
+++ ImageGeneratorApp/src/test/java/layers/web/vaadin/ImageGeneratorPageObject.java
@@ -3,13 +3,10 @@ package layers.web.vaadin;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-import system.ResourceReader;
 
 import java.net.URL;
-import java.util.Properties;
 
 import static com.codeborne.selenide.Selenide.$;
-import static java.lang.Integer.valueOf;
 import static layers.web.vaadin.layout.footer.FooterLayout.FLAGS_LINK_ID;
 import static layers.web.vaadin.layout.footer.FooterLayout.JAVARUSH_LINK_ID;
 import static layers.web.vaadin.layout.header.HeaderLayout.CODACY_LINK_ID;
@@ -17,21 +14,21 @@ import static layers.web.vaadin.layout.header.HeaderLayout.GITHUB_LINK_ID;
 
 public class ImageGeneratorPageObject {
 
-    public ImageGeneratorPageObject() {
-        Configuration.browser = "chrome";
+    private String protocol;
+    private String host;
+    private Integer port;
+    private String route;
+
+    public ImageGeneratorPageObject(String browser, String protocol, String host, Integer port, String route) {
+        Configuration.browser = browser;
+
+        this.protocol = protocol;
+        this.host     = host;
+        this.port     = port;
+        this.route    = route;
     }
 
     public URL url() throws Exception {
-        URL propertiesURL     = new ResourceReader().readURL("webapp-integration-tests.properties");
-        Properties properties = new Properties();
-
-        properties.load(propertiesURL.openStream());
-
-        String protocol = properties.getProperty("webapp.protocol");
-        String host     = properties.getProperty("webapp.host");
-        Integer port    = valueOf(properties.getProperty("webapp.port"));
-        String route    = "/" + properties.getProperty("webapp.route");
-
         return new URL(protocol, host, port, route);
     }
 
--- ImageGeneratorApp/src/test/java/layers/web/vaadin/layout/footer/FooterLayoutUIIT.java
+++ ImageGeneratorApp/src/test/java/layers/web/vaadin/layout/footer/FooterLayoutUIIT.java
@@ -3,15 +3,22 @@ package layers.web.vaadin.layout.footer;
 import layers.web.vaadin.ImageGeneratorPageObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.title;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
 public class FooterLayoutUIIT {
 
-    private ImageGeneratorPageObject imageGeneratorPage = new ImageGeneratorPageObject();
+    @Autowired
+    private ImageGeneratorPageObject imageGeneratorPage;
 
     @Before
     public void setUp() throws Exception {
